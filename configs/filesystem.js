a698ff6d6aef0565ca3692b7cd1554687ad1d7c0
cloud <565548320@qq.com>
2020-01-21 16:05:51 +0800

    Update filesystem.js

--- configs/filesystem.js
+++ configs/filesystem.js
@@ -2,8 +2,8 @@ module.exports = {
   oss: {
     region: 'oss-cn-beijing',
     // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，部署在服务端使用RAM子账号或STS，部署在客户端使用STS。
-    accessKeyId: 'LTAIfQ3CdLGwFOqK',
-    accessKeySecret: 'Qs3TwmFFZ15B8XQuLs12QZBfyJ05JfX',
+    accessKeyId: 'LTAIfQ3CdL2GwFOqK',
+    accessKeySecret: 'Qs3T2wmFFZ115B8XQuLs12QZBfyJ05JfX',
     bucket: 'parallel-dev'
   }
 }
